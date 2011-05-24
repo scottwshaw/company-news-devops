62b22d80366470aac51f75200a654ef94d4cf36b
Scott Shaw <scottwshaw@gmail.com>
2011-05-25 06:35:00 +1000

    partially done with exercise, checking in intermediate results

--- puppet/files/deploy.rb
+++ puppet/files/deploy.rb
@@ -11,7 +11,12 @@ module MCollective
 
       action "war" do
         validate :source, String
-
+        `wget #{request[:source]}`
+        `sudo service tomcat6 stop`
+        `rm -r /var/lib/tomcat6/webapps/*`
+        `mv companyNews.war /var/lib/tomcat6/webapps`
+        `sudo service tomcat6 start`
+        reply[:message] = "Completed"
       end
     end
   end
--- puppet/manifests/app.pp
+++ puppet/manifests/app.pp
@@ -2,6 +2,21 @@ import "base.pp"
 
 class app inherits base {
 
+  file { "/var/db/prevaylor":
+    ensure => "directory",
+    owner => "tomcat",
+    require => Package["tomcat6"],
+  }
+
+  package { "tomcat6":
+    ensure => "installed",
+  }
+
+  file { "/etc/tomcat6/tomcat6.conf":
+    require => Package["tomcat6"],
+    source => "file://${files_dir}/tomcat6.conf",
+  }    
+
 }
 
 include app
